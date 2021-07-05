4640ba12eb4d5c2c20e98ccbdad7d6af7aeddfd7
Mnevis-WebDev <86825299+Mnevis-WebDev@users.noreply.github.com>
2021-07-05 10:03:29 -0400

    Inline CSS

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const fName = "Mnevis";
 const lName = "WebDev";
 var lnum = Math.round(Math.random() * 100);
 
+//CSS inline
+const customStyle = {
+  color: "blue",
+  border: "solid",
+  fontSize: "30px"
+};
+
 ReactDOM.render(
   <div>
-    <h1>
+    <h1 style={customStyle}>
       Hello {fName}, {lName}!
     </h1>
     <p>your lucky number is {lnum} </p>
+    <h1 style={{ fontSize: "10px" }}>This is another test</h1>
   </div>,
   document.getElementById("root")
 );
