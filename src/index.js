999c508b5725a627c1422c287c97a7f60c4ab0d5
Mnevis-WebDev <86825299+Mnevis-WebDev@users.noreply.github.com>
2021-07-05 01:53:31 -0400

    Add a h1 element This is just a test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ ReactDOM.render(
   <div>
     <h1>Hello World</h1>
     <p>Bla bla bla</p>
+    <p>this is just to test the commit</p>
+    <h1> I was unable to see the change </h1>
   </div>,
   document.getElementById("root")
 );
