ca961cb38483daefeb16c26d0d66fbc9583ff179
Mnevis-WebDev <86825299+Mnevis-WebDev@users.noreply.github.com>
2021-07-06 01:25:13 -0400

    Module importation

---
+++ src/components/calculator.js
@@ -0,0 +1,18 @@
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function soustract(n1, n2) {
+  return n1 - n2;
+}
+
+export default multiply;
+export { divide, add, soustract };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-
+import * as Calc from "./components/calculator.js";
+console.log(Calc);
 ReactDOM.render(
   <div>
     <App />
+
+    <ul>
+      <li>{Calc.default(2, 4)}</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
